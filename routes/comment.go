5f4d8431b76d5dcc7014174eb5ba49cc62fedc66
dickson7 <garciadickson@hotmail.com>
2019-04-14 18:22:36 -0500

    se agrega la ruta y se inicializa en la ruta principal

---
+++ routes/comment.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"github.com/dickson7/proyectogocomentarios/controllers"
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+)
+
+// SetCommentRouter ruta para comentarios
+func SetCommentRouter(router *mux.Router) {
+	prefix := "/api/comments"
+	subRouter := mux.NewRouter().PathPrefix(prefix).Subrouter().StrictSlash(true)
+	subRouter.HandleFunc("/", controllers.CommentCreate).Methods("POST")
+
+	//validamos el token
+	router.PathPrefix(prefix).Handler(
+		negroni.New(
+			negroni.HandlerFunc(controllers.ValidateToken),
+			negroni.Wrap(subRouter),
+		),
+	)
+}
